5b888bf65594a5765070faa89bd8a6cebddee28e
Haikal Siregar <mininginformation1337@gmail.com>
2025-12-06 09:45:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express()
 const PORT = process.env.PORT || 4444
 
 app.use(cors({
-  origin: ["http://localhost:5739", "http://localhost", "http://127.0.0.1:5739, "*"], // domain Flutter Web kamu
+  origin: ["http://localhost:5739", "http://localhost", "http://127.0.0.1:5739", "*"], // domain Flutter Web kamu
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }))
